4d8b65b289061e4e2ec74443b0e07aefbf5fe081
Timur Nurdinov <tnurdinov@i-novus.ru>
2018-08-08 10:30:58 +0300

    bf NNOP-3  test fix

--- n2o-platform-jaxrs/n2o-platform-jaxrs-autoconfigure/src/test/java/net/n2oapp/platform/jaxrs/JaxRsClientTest.java
+++ n2o-platform-jaxrs/n2o-platform-jaxrs-autoconfigure/src/test/java/net/n2oapp/platform/jaxrs/JaxRsClientTest.java
@@ -172,7 +172,7 @@ public class JaxRsClientTest {
             RestException restException = (RestException) e;
             assertThat(restException.getErrors().size(), equalTo(3));
             List<String> errorTextList = restException.getErrors().stream().map(RestMessage.Error::getMessage).collect(Collectors.toList());
-            assertThat(errorTextList, hasItems("Ошибка пользователя раз", "Ошибка пользователя два", "Другая ошибка пользователя"));
+            assertThat(errorTextList, anyOf(hasItems("Ошибка пользователя раз", "Ошибка пользователя два", "Другая ошибка пользователя"), hasItems("user.error1", "user.error1", "user.error2")));
         }
     }
 
