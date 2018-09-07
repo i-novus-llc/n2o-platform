713825b136b78533074879ad0b1b408b0f2fc112
rmakhmutov <rmakhmutov@i-novus.ru>
2018-09-07 14:55:00 +0300

    nf NNOP-5 added monitoring feature enabled by default in platform-based applications

---
+++ n2o-platform-actuator/n2o-platform-actuator-autoconfigure/src/main/java/net/n2oapp/platform/actuate/health/KafkaHealthIndicator.java
@@ -0,0 +1,28 @@
+package net.n2oapp.platform.actuate.health;
+
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.util.Assert;
+
+public class KafkaHealthIndicator extends AbstractHealthIndicator {
+
+    private final KafkaTemplate kafkaTemplate;
+
+    public KafkaHealthIndicator(KafkaTemplate kafkaTemplate) {
+        Assert.notNull(kafkaTemplate, "kafkaTemplate must not be null");
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+        try {
+            kafkaTemplate.send("test", null).get();
+            builder.up();
+        }
+        catch (Exception ex)
+        {
+            builder.down(ex);
+        }
+    }
+}
---
+++ n2o-platform-actuator/n2o-platform-actuator-autoconfigure/src/test/java/net/n2oapp/platform/actuate/autoconfigure/ActuatorAutoConfigurationTest.java
@@ -0,0 +1,40 @@
+package net.n2oapp.platform.actuate.autoconfigure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author RMakhmutov
+ * @since 06.09.2018
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=ActuatorAutoConfigurationTest.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration
+public class ActuatorAutoConfigurationTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testActuator() {
+        ActuatorHealthResponse response = restTemplate.getForObject(ActuatorAutoConfiguration.ACTUATOR_CONTEXT_PATH + "/health", ActuatorHealthResponse.class);
+        assert response.getStatus().equals("UP");
+    }
+
+    private static class ActuatorHealthResponse {
+        private String status;
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+    }
+}
