805f2169314e4ca3482ebb0615c80bb08d99051e
iryabov <iryabov@i-novus.ru>
2019-01-29 09:06:40 +0300

    [NNOP-16] [RF] Удалил deprecated код по требованию sonar

--- n2o-platform-i18n/n2o-platform-i18n-commons/src/main/java/net/n2oapp/platform/i18n/UserException.java
+++ n2o-platform-i18n/n2o-platform-i18n-commons/src/main/java/net/n2oapp/platform/i18n/UserException.java
@@ -1,7 +1,5 @@
 package net.n2oapp.platform.i18n;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,14 +9,7 @@ import java.util.stream.Collectors;
  */
 public class UserException extends RuntimeException {
     private static final long serialVersionUID = 2552353701499979545L;
-    /**
-     * @deprecated Общие сообщения уже есть на клиенте при возникновении любой ошибки. Для этого выбрасывать это исключение не нужно.
-     */
-    @Deprecated(forRemoval = true, since = "2.2")
-    private static final Message UNEXPECTED_ERROR = new Message("exception.unexpectedError");
-
     private transient Object[] args;
-
     private final transient List<Message> messages;
 
     public UserException(Message message) {
@@ -29,7 +20,7 @@ public class UserException extends RuntimeException {
 
     public UserException(List<Message> messages) {
         super(messages.stream().map(Message::getCode).collect(Collectors.joining(",\n")));
-        this.messages = messages;
+        this.messages = Collections.unmodifiableList(messages);
     }
 
 
@@ -49,45 +40,12 @@ public class UserException extends RuntimeException {
         this.messages = null;
     }
 
-    /**
-     * @deprecated Это исключение предназначено для передачи сообщений пользователю. Например, для валидаций.
-     * Непредвиденные ошибки не должны бросать это исключение.
-     */
-    @Deprecated(forRemoval = true, since = "2.2")
-    public UserException() {
-        super();
-        this.messages = null;
-    }
-
-    /**
-     * @deprecated Это исключение предназначено для передачи сообщений пользователю. Например, для валидаций.
-     * Непредвиденные ошибки не должны бросать это исключение.
-     */
-    @Deprecated(forRemoval = true, since = "2.2")
-    public UserException(Throwable cause) {
-        super(cause);
-        this.messages = null;
-    }
-
     public Object[] getArgs() {
         return this.args;
     }
 
     public String getCode() {
         return getMessage();
-    }
-
-    /**
-     * Добавить параметр сообщения
-     * @param argument Параметр
-     * @deprecated Исключения должны быть немодифицируемы
-     */
-    @Deprecated(forRemoval = true, since = "2.2")
-    public UserException set(Object argument) {
-        ArrayList<Object> list = new ArrayList<>(args != null ? Arrays.asList(args) : Collections.emptyList());
-        list.add(argument);
-        this.args = list.toArray();
-        return this;
     }
 
     public List<Message> getMessages() {
