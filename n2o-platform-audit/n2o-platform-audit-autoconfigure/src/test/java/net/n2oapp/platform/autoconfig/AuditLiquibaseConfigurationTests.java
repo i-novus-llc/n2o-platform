22100a2422e7fa086f7b8851538d67d79aff6b74
asamoilov <13865>
2019-03-26 12:28:26 +0300

    СF DEVENV-216 marge request discussion resolve

--- n2o-platform-audit/n2o-platform-audit-autoconfigure/src/test/java/net/n2oapp/platform/autoconfig/AuditLiquibaseConfigurationTests.java
+++ n2o-platform-audit/n2o-platform-audit-autoconfigure/src/test/java/net/n2oapp/platform/autoconfig/AuditLiquibaseConfigurationTests.java
@@ -7,18 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.lang.Nullable;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 @SpringBootApplication
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = {AuditLiquibaseConfigurationTests.class, H2JpaConfig.class, AuditLiquibaseConfiguration.class})
+@SpringBootTest(classes = {AuditLiquibaseConfigurationTests.class, AuditLiquibaseConfiguration.class})
 public class AuditLiquibaseConfigurationTests {
-    @PersistenceContext
-    EntityManager entityManager;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
     @Autowired
     ApplicationContext context;
 
@@ -34,7 +32,7 @@ public class AuditLiquibaseConfigurationTests {
 
     @Test
     public void testDBChangelog() {
-        Assert.assertTrue((Boolean) entityManager.createNativeQuery("SELECT EXISTS (SELECT 1 FROM information_schema.tables " +
-                "WHERE  table_schema = 'PUBLIC' AND table_name = 'DATABASECHANGELOG')").getSingleResult());
+        Assert.assertTrue(jdbcTemplate.queryForObject("SELECT EXISTS (SELECT 1 FROM information_schema.tables " +
+                "WHERE  table_schema = 'PUBLIC' AND table_name = 'DATABASECHANGELOG')", Boolean.class));
     }
 }
