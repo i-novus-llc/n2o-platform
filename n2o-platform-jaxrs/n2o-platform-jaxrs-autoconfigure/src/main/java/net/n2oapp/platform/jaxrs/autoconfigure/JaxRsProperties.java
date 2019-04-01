a5841e2a0d72e5aaeb2490b562d83a0e84b990a5
lgalimova <lgalimova@i-novus.ru>
2019-04-01 12:46:49 +0300

    nf NNOP-35 Пробрасывать максимум настроек которые предоставляет cxf

--- n2o-platform-jaxrs/n2o-platform-jaxrs-autoconfigure/src/main/java/net/n2oapp/platform/jaxrs/autoconfigure/JaxRsProperties.java
+++ n2o-platform-jaxrs/n2o-platform-jaxrs-autoconfigure/src/main/java/net/n2oapp/platform/jaxrs/autoconfigure/JaxRsProperties.java
@@ -10,11 +10,73 @@ public class JaxRsProperties {
     /**
      * Логирование всех запросов
      */
+    @Deprecated
     private boolean logIn = true;
+    private Logging loggingIn = new Logging();
     /**
      * Логирование всех ответов
      */
+    @Deprecated
     private boolean logOut = true;
+    private Logging loggingOut = new Logging();
+
+    public static class Logging {
+        private boolean enabled = true;
+        private int limit = 1024 * 1024;
+        private long inMemThreshold = 100 * 1024;
+        private boolean prettyLogging;
+        private boolean logBinary;
+        private boolean logMultipart = true;
+
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
+        }
+
+        public int getLimit() {
+            return limit;
+        }
+
+        public void setLimit(int limit) {
+            this.limit = limit;
+        }
+
+        public long getInMemThreshold() {
+            return inMemThreshold;
+        }
+
+        public void setInMemThreshold(long inMemThreshold) {
+            this.inMemThreshold = inMemThreshold;
+        }
+
+        public boolean isPrettyLogging() {
+            return prettyLogging;
+        }
+
+        public void setPrettyLogging(boolean prettyLogging) {
+            this.prettyLogging = prettyLogging;
+        }
+
+        public boolean isLogBinary() {
+            return logBinary;
+        }
+
+        public void setLogBinary(boolean logBinary) {
+            this.logBinary = logBinary;
+        }
+
+        public boolean isLogMultipart() {
+            return logMultipart;
+        }
+
+        public void setLogMultipart(boolean logMultipart) {
+            this.logMultipart = logMultipart;
+        }
+    }
+
     /**
      * Включение валидаций JSR303
      */
@@ -91,6 +153,14 @@ public class JaxRsProperties {
 
     public void setLogOut(boolean logOut) {
         this.logOut = logOut;
+    }
+
+    public Logging getLoggingIn() {
+        return loggingIn;
+    }
+
+    public Logging getLoggingOut() {
+        return loggingOut;
     }
 
     public boolean isJsr303() {
