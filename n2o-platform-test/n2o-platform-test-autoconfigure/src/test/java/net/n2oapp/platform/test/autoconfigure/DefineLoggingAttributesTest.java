c98a7afca3c0a7d4127cac76d773a59ab907571c
lgalimova <lgalimova@i-novus.ru>
2019-04-02 14:23:26 +0300

    nf NNOP-35 Тест

--- n2o-platform-test/n2o-platform-test-autoconfigure/src/test/java/net/n2oapp/platform/test/autoconfigure/DefineLoggingAttributesTest.java
+++ n2o-platform-test/n2o-platform-test-autoconfigure/src/test/java/net/n2oapp/platform/test/autoconfigure/DefineLoggingAttributesTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.lang.reflect.Field;
