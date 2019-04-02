90f2d0fd3b3aebf6d1162f0218971bca37678e6a
lgalimova <lgalimova@i-novus.ru>
2019-04-02 14:23:18 +0300

    nf NNOP-35 Тест

--- n2o-platform-test/n2o-platform-test-autoconfigure/src/test/java/net/n2oapp/platform/test/autoconfigure/DefineLoggingAttributesTest.java
+++ n2o-platform-test/n2o-platform-test-autoconfigure/src/test/java/net/n2oapp/platform/test/autoconfigure/DefineLoggingAttributesTest.java
@@ -35,7 +35,6 @@ import static org.junit.Assert.assertEquals;
 public class DefineLoggingAttributesTest {
 
     @Autowired
-    @SpyBean
     private LoggingInInterceptor loggingInInterceptor;
 
     @Test
