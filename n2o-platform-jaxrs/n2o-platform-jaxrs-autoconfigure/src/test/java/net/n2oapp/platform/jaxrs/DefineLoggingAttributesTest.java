b8bbb2e739940cef7e4dbdcd882c892171233878
lgalimova <lgalimova@i-novus.ru>
2019-04-09 12:43:34 +0300

    nf NNOP-35 Перенесла тесты

--- n2o-platform-jaxrs/n2o-platform-jaxrs-autoconfigure/src/test/java/net/n2oapp/platform/jaxrs/DefineLoggingAttributesTest.java
+++ n2o-platform-jaxrs/n2o-platform-jaxrs-autoconfigure/src/test/java/net/n2oapp/platform/jaxrs/DefineLoggingAttributesTest.java
@@ -7,7 +7,6 @@ import org.apache.cxf.ext.logging.event.PrettyLoggingFilter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
