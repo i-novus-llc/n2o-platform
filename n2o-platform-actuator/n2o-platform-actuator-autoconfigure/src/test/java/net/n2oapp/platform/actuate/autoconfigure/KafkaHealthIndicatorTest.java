398da3bb237ce03dbc076589a3014ef52aae4470
rmakhmutov <rmakhmutov@i-novus.ru>
2019-06-10 14:17:28 +0300

    bf NNOP-48 trying to fix kafka healthcheck

--- n2o-platform-actuator/n2o-platform-actuator-autoconfigure/src/test/java/net/n2oapp/platform/actuate/autoconfigure/KafkaHealthIndicatorTest.java
+++ n2o-platform-actuator/n2o-platform-actuator-autoconfigure/src/test/java/net/n2oapp/platform/actuate/autoconfigure/KafkaHealthIndicatorTest.java
@@ -4,11 +4,14 @@ import net.n2oapp.platform.actuate.health.KafkaHealthIndicator;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.*;
+import org.junit.runner.RunWith;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.Status;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.SocketUtils;
 
 import java.util.HashMap;
@@ -18,17 +21,17 @@ import java.util.Map;
  * @author RMakhmutov
  * @since 06.09.2018
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = KafkaHealthIndicatorTest.class)
 public class KafkaHealthIndicatorTest {
     @ClassRule
     public static EmbeddedKafkaRule kafkaEmbedded = new EmbeddedKafkaRule(1);
 
-    private KafkaTemplate kafkaTemplate;
-
     @Test
     public void kafkaIsUp() throws Exception {
-        initKafkaTemplate(this.kafkaEmbedded.getEmbeddedKafka().getBrokersAsString());
+        KafkaTemplate kafkaTemplate = initKafkaTemplate(this.kafkaEmbedded.getEmbeddedKafka().getBrokersAsString());
 
-        KafkaHealthIndicator healthIndicator = new KafkaHealthIndicator(this.kafkaTemplate);
+        KafkaHealthIndicator healthIndicator = new KafkaHealthIndicator(kafkaTemplate);
         Health health = healthIndicator.health();
 
         assert health.getStatus() == Status.UP;
@@ -36,20 +39,20 @@ public class KafkaHealthIndicatorTest {
 
     @Test
     public void kafkaIsDown() {
-        initKafkaTemplate("127.0.0.1:" + SocketUtils.findAvailableTcpPort());
+        KafkaTemplate kafkaTemplate = initKafkaTemplate("127.0.0.1:" + SocketUtils.findAvailableTcpPort());
 
-        KafkaHealthIndicator healthIndicator = new KafkaHealthIndicator(this.kafkaTemplate);
+        KafkaHealthIndicator healthIndicator = new KafkaHealthIndicator(kafkaTemplate);
         Health health = healthIndicator.health();
 
         assert health.getStatus() == Status.DOWN;
     }
 
-    private void initKafkaTemplate(String bootstrapServers) {
+    private KafkaTemplate initKafkaTemplate(String bootstrapServers) {
         Map<String, Object> config = new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 1000); /// timeout reduced for testing speed up
-        this.kafkaTemplate = new KafkaTemplate(new DefaultKafkaProducerFactory(config));
+        return new KafkaTemplate(new DefaultKafkaProducerFactory(config));
     }
 }
