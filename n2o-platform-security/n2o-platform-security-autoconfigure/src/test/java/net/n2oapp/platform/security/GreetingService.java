c70c621e60a058b8d3ebe730d8e748a15021edd2
iryabov <iryabov@i-novus.ru>
2019-07-16 10:55:02 +0300

    [NNOP-44] [RF] Лишний импорт

--- n2o-platform-security/n2o-platform-security-autoconfigure/src/test/java/net/n2oapp/platform/security/GreetingService.java
+++ n2o-platform-security/n2o-platform-security-autoconfigure/src/test/java/net/n2oapp/platform/security/GreetingService.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.websocket.server.PathParam;
-
 @RestController
 public class GreetingService {
     @Autowired
