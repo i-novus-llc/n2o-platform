3de10df6630ce11eb5f2ef4876759ed610382ccd
Молоствов Игорь <imolostvov@i-novus.ru>
2020-12-08 17:10:41 +0300

    [CF][NNOP-172] Обновление access_token в JaxRsJwtHeaderInterceptor

--- n2o-platform-web/n2o-platform-web-autoconfigure/src/main/java/net/n2oapp/platform/web/autoconfigure/JaxRsJwtHeaderInterceptor.java
+++ n2o-platform-web/n2o-platform-web-autoconfigure/src/main/java/net/n2oapp/platform/web/autoconfigure/JaxRsJwtHeaderInterceptor.java
@@ -6,6 +6,7 @@ import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.util.StringUtils;
 
@@ -22,6 +23,8 @@ public class JaxRsJwtHeaderInterceptor extends AbstractPhaseInterceptor<Message>
 
     @Autowired(required = false)
     private OAuth2ClientContext oauth2ClientContext;
+    @Autowired(required = false)
+    private OAuth2RestTemplate restTemplate;
 
     public JaxRsJwtHeaderInterceptor() {
         super(Phase.WRITE);
@@ -30,15 +33,15 @@ public class JaxRsJwtHeaderInterceptor extends AbstractPhaseInterceptor<Message>
     @SuppressWarnings("unchecked")
     @Override
     public void handleMessage(Message message) {
-        if (oauth2ClientContext != null
-                && oauth2ClientContext.getAccessToken() != null) {
-            String tokenType = oauth2ClientContext.getAccessToken().getTokenType();
+        if (oauth2ClientContext != null && oauth2ClientContext.getAccessToken() != null) {
+            OAuth2AccessToken accessToken = restTemplate.getAccessToken();
+            String tokenType = accessToken.getTokenType();
             if (!StringUtils.hasText(tokenType)) {
                 tokenType = OAuth2AccessToken.BEARER_TYPE;
             }
             Map<String, List> headers = (Map<String, List>) message.get("org.apache.cxf.message.Message.PROTOCOL_HEADERS");
             headers.put("Authorization", Collections.singletonList(String.format("%s %s", tokenType,
-                    oauth2ClientContext.getAccessToken().getValue())));
+                    accessToken.getValue())));
         }
     }
 }
