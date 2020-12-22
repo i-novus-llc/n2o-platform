2f223458bc804e94f2c1ada1230bc715c0cdada5
ikarimullin <ikarimullin@i-novus.ru>
2020-12-22 09:05:52 +0300

    nf NNOP-175 Selection API

--- n2o-platform-selection-api/n2o-platform-selection-api-core/src/test/java/net/n2oapp/platform/selection/core/selection/FoodSelection.java
+++ n2o-platform-selection-api/n2o-platform-selection-api-core/src/test/java/net/n2oapp/platform/selection/core/selection/FoodSelection.java
@@ -1,7 +1,16 @@
 package net.n2oapp.platform.selection.core.selection;
 
 import net.n2oapp.platform.selection.core.Selection;
+import net.n2oapp.platform.selection.core.SelectionEnum;
+import net.n2oapp.platform.selection.core.SelectionKey;
 import net.n2oapp.platform.selection.core.model.Food;
 
 public interface FoodSelection<E extends Food> extends Selection<Food> {
+
+    @SelectionKey("name")
+    SelectionEnum selectName(E model);
+
+    @SelectionKey("color")
+    SelectionEnum selectColor(E model);
+
 }
