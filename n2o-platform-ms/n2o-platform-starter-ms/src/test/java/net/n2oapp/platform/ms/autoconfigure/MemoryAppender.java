df9ddf774d954d3061062593ad11227e92e3b46b
anigmatullin <anigmatullin@i-novus.ru>
2024-06-21 17:23:49 +0300

    JSON format logs
    https://jira.i-novus.ru/browse/EGISZNRPLATFORM-1639
    See merge request platform/n2o!180
    
    (cherry picked from commit ae6522fe3eef18da4ebaf03c251c1ade3a7c1356)
    
    d660970e [NF][EGISZNRPLATFORM-1639] - Добавить json формат логов
    0e293c50 [NF][EGISZNRPLATFORM-1639] - Добавить json формат логов
    835abb2b [NF][EGISZNRPLATFORM-1639] - Добавить json формат логов
    71c29283 [NF][EGISZNRPLATFORM-1639] - Добавить json формат логов
    29c2cb2a [NF][EGISZNRPLATFORM-1639] - Добавить json формат логов
    9f46fdec [NF][EGISZNRPLATFORM-1639] - Добавить json формат логов
    3fac6a8d [NF][EGISZNRPLATFORM-1639] - Добавить json формат логов
    c058e871 [NF][EGISZNRPLATFORM-1639] - Добавить json формат логов
    4fd3939f [NF][EGISZNRPLATFORM-1639] - Добавить json формат логов
    60598539 [NF][EGISZNRPLATFORM-1639] - Добавить json формат логов
    3897f43b Merge branch 'refs/heads/release/5.2.x' into sub/EGISZNRPLATFORM-1639
    9e696e24 [NF][EGISZNRPLATFORM-1639] - Добавить json формат логов

--- n2o-platform-ms/n2o-platform-starter-ms/src/test/java/net/n2oapp/platform/ms/autoconfigure/MemoryAppender.java
+++ n2o-platform-ms/n2o-platform-starter-ms/src/test/java/net/n2oapp/platform/ms/autoconfigure/MemoryAppender.java
@@ -7,7 +7,13 @@ import ch.qos.logback.core.read.ListAppender;
 public class MemoryAppender extends ListAppender<ILoggingEvent> {
     public boolean contains(String string, Level level) {
         return this.list.stream()
-                .anyMatch(event -> event.getMessage().toString().contains(string)
+                .anyMatch(event -> event.getMessage().contains(string)
                         && event.getLevel().equals(level));
+    }
+
+    public ILoggingEvent findFirst(String string) {
+        return this.list.stream()
+                .filter(event -> event.toString().contains(string))
+                .findFirst().orElse(null);
     }
 }
