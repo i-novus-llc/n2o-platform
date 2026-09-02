4e19667ac13c905a28ab78257f05d2a4b77430ea
anikolaev <anikolaev@i-novus.ru>
2026-09-02 18:49:07 +0300

    added suppress warnings java:S3011 for reflection methods fix

--- n2o-platform-seek-paging/src/main/java/net/n2oapp/platform/seek/SeekableRepositoryFactory.java
+++ n2o-platform-seek-paging/src/main/java/net/n2oapp/platform/seek/SeekableRepositoryFactory.java
@@ -1,16 +1,14 @@
 package net.n2oapp.platform.seek;
 
+import org.springframework.data.jpa.repository.support.CrudMethodMetadata;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
-import org.springframework.data.jpa.repository.support.QuerydslJpaPredicateExecutor;
-import org.springframework.data.querydsl.SimpleEntityPathResolver;
+import org.springframework.data.querydsl.EntityPathResolver;
 import org.springframework.data.repository.core.RepositoryMetadata;
 import org.springframework.data.repository.core.support.RepositoryComposition;
-import org.springframework.data.repository.core.support.RepositoryFragment;
 import org.springframework.lang.NonNull;
+
 import jakarta.persistence.EntityManager;
-import java.lang.reflect.Field;
-import java.util.Optional;
 
 /**
  * Данный класс сохраняет полную совместимость с {@link JpaRepositoryFactory},
@@ -23,32 +21,24 @@ public class SeekableRepositoryFactory extends JpaRepositoryFactory {
     }
 
     @Override
-    protected @NonNull RepositoryComposition.RepositoryFragments getRepositoryFragments(@NonNull RepositoryMetadata metadata) {
-        RepositoryComposition.RepositoryFragments fragments = super.getRepositoryFragments(metadata);
-        RepositoryComposition.RepositoryFragments modifiedFragment = RepositoryComposition.RepositoryFragments.empty();
-        for (RepositoryFragment<?> fragment : fragments) {
-            Optional<?> implementation = fragment.getImplementation();
-            if (implementation.isEmpty() || implementation.get().getClass() != QuerydslJpaPredicateExecutor.class) {
-                modifiedFragment = modifiedFragment.append(fragment);
-            } else {
-                JpaEntityInformation<?, Object> entityInformation = getEntityInformation(metadata.getDomainType());
-                boolean isSeekable = SeekableRepository.class.isAssignableFrom(metadata.getRepositoryInterface());
-                if (isSeekable) {
-                    Object querydsl = implementation.get();
-                    modifiedFragment = modifiedFragment.append(
-                        RepositoryFragment.implemented(
-                            getTargetRepositoryViaReflection(
-                                SeekableRepositoryImpl.class,
-                                entityInformation,
-                                SimpleEntityPathResolver.INSTANCE,
-                                metadata.getRepositoryInterface()
-                            )
-                        )
-                    );
-                } else
-                    modifiedFragment = modifiedFragment.append(fragment);
-            }
+    protected @NonNull RepositoryComposition.RepositoryFragments getRepositoryFragments(
+        @NonNull RepositoryMetadata metadata,
+        @NonNull EntityManager entityManager,
+        @NonNull EntityPathResolver resolver,
+        @NonNull CrudMethodMetadata crudMethodMetadata
+    ) {
+        if (!SeekableRepository.class.isAssignableFrom(metadata.getRepositoryInterface())) {
+            return super.getRepositoryFragments(metadata, entityManager, resolver, crudMethodMetadata);
         }
-        return modifiedFragment;
+        JpaEntityInformation<?, Object> entityInformation = getEntityInformation(metadata.getDomainType());
+        return RepositoryComposition.RepositoryFragments.just(
+            new SeekableRepositoryImpl<>(
+                entityInformation,
+                entityManager,
+                resolver,
+                crudMethodMetadata,
+                metadata.getRepositoryInterface()
+            )
+        );
     }
 }
